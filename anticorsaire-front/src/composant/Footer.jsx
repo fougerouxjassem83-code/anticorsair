a00079ae6b5f09b13c1587d5a4d60357b71e571f
ikraamanlipro-dev <ikraamanli.pro@gmail.com>
2026-06-08 16:25:16 +0300

    Création du composant Footer

---
+++ anticorsaire-front/src/composant/Footer.jsx
@@ -0,0 +1,13 @@
+/**
+ *  Ce composant représente la section de pied de page de notre application. Il contient des informations de contact, des liens vers les réseaux sociaux et d'autres éléments pertinents pour les utilisateurs. Nous allons styliser ce composant pour qu'il soit visuellement attrayant et cohérent avec le reste de l'application.
+ */
+
+const Footer = () =>{
+    return (
+        <div className="footer">
+            <p>Contactez-nous : info@compagnie-voyage.com</p>
+        </div>
+    )
+}
+
+export default Footer
