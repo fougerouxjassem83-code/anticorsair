6e76da097e733c0b903f1b5cd7b012aa50c7bfba
ikraamanlipro-dev <ikraamanli.pro@gmail.com>
2026-06-09 16:23:57 +0300

    Création de la page Réservation

---
+++ anticorsaire-front/src/page/Reservation.jsx
@@ -0,0 +1,9 @@
+// Dans se fichier on va pouvoir faire des reservations de vols et voir les réservations déjà faites par les clients
+
+const Reservation = () => {
+    return (
+        <h2>Page de réservation</h2>
+    )
+}
+
+export default Reservation
